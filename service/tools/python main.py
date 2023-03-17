81685a958873b376ab75726cbc35edc387650a68
jijiang hou <113097833+daheitian@users.noreply.github.com>
2023-03-17 17:58:51 +0800

    Create python main.py

---
+++ service/tools/python main.py
@@ -0,0 +1,3 @@
+# 进入文件夹 `/service` 运行以下命令
+python main.py --openai_api_key="secret_L8SXYBjBQDH3Z7elpalEiU6mOWrfaEE1qlSAesBMN7D" --api_model="$API_MODEL" --socks_proxy="$SOCKS_PROXY" --host="$HOST" --port="$PORT"
+# 后端服务的默认端口号是3002，可以通过 --port 参数修改
